34aeb05d7a691c079308ad114796f313477bed2f
Marwin <56474502+TinkerPal99@users.noreply.github.com>
2020-01-30 18:44:03 +0100

    deployed request_dispatcher
    deployed request_dispatcher and some final fixes

--- request_dipatcher.py
+++ request_dipatcher.py
@@ -1,4 +1,4 @@
-from Dispatcher.library import Priorisator, job
+from library import Priorisator, job
 import socket
 
 import logging
@@ -6,7 +6,7 @@ import sys
 
 # ---------------------------logger-------------------------------------------------------------------------------------
 logging.basicConfig(
-    filename="logs\dispatcher_log.log",
+    filename="dispatcher_log.log",
     filemode="a",
     format="%(asctime)s|%(process)d||%(levelname)s|-|%(message)s")
 
@@ -25,33 +25,45 @@ request_dispatcher = Priorisator.Priorisator()
 s = socket.socket()
 host = '192.168.0.104'
 port = 9900
-client = None
+
+s.bind((host, port))
+s.listen()
 adress = None
+client = None
 
 
-def is_connected():
-    # @is_connected scannt ob gerät ich verbinden will, akzeptiert dieses. Sendet bestätigung an verbinder
-    global client, adress
+def __is_connected():
+    # @is_connected scannt ob geraet sich verbinden will, akzeptiert dieses. Sendet bestaetigung an verbinder
+    global adress, client
 
     while True:
         client, adress = s.accept()
-        if adress != "":
+        if adress is None:
             nachricht = "Connection to EZB-Priorisator established.".encode("ascii")
             logging.info("Connected to " + str(adress))
-            client.send(nachricht)
-            return True
+            try:
+                client.send(nachricht)
+            except ConnectionAbortedError:
+                logging.error("Connection was aborted", exc_info=True)
+            except ConnectionError and ConnectionRefusedError:
+                logging.error("ConnectionError", exc_info=True)
+            else:
+                logging.info("Build connection to " + adress)
+                return True
+        else:
+            return False
 
 
-def receive_request():
+
+def __receive_request():
     # empfängt jobpaket und steckt es in die queue
     newJob = job.job()
-    newJob.sync(client.recv(1024))
-    request_dispatcher.put(newJob)
+    request_dispatcher.put(newJob.sync(client.recv(1024)))
     request_dispatcher.organize()
     logging.info("Received packet from " + str(adress))
 
 
-def sendjob():
+def __sendjob():
     # nimmt job mit der höchsten Prio aus der joblist und sendet ihn an den Wagen der in der Destination angegeben ist
     s_dest = socket.socket()
     job_item = request_dispatcher.get()
@@ -63,7 +75,8 @@ def sendjob():
         s.send(job_item.encode("ascii"))
     except ConnectionRefusedError:
         logging.warning("Connection Refused", exc_info=True)
-        SystemExit(2)
+    except ConnectionError and ConnectionAbortedError:
+        logging.error("ConnectionError", exc_info=True)
     except OSError:
         logging.critical("Servererror", exc_info=True)
         SystemExit(2)
@@ -72,6 +85,6 @@ def sendjob():
 
 
 while True:
-    if is_connected():
-        receive_request()
-        sendjob()
+    if __is_connected():
+        __receive_request()
+        __sendjob()
